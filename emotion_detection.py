5d6f491753434a7bdaa6e4ad2479247b90816d34
bentsanghoilun <ben.tsanghoilun@gmail.com>
2026-03-08 07:16:36 -0400

    updating emotion detector

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,21 @@
+import requests
+import json
 
-def emotion_detector():
+def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    inputJson = { "raw_document": { "text": text_to_analyse } }
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = myobj, headers=header)
+
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotions, key=emotions.get)
+        
+        output = dict(emotions) 
+        output['dominant_emotion'] = dominant_emotion
+    else:
+        output = "Unexpected error"
+
+    return output
